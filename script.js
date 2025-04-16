3f75cebbc27b446cebceeb06146db619c8b10247
SkorpionG2000 <105552971+SkorpionG@users.noreply.github.com>
2025-04-15 18:04:17 -0700

    Add more professional website pages
    Added the following page with improved styling:
    - About
    - Reservation
    - Member Login/Register
    - Contact

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const year = new Date().getFullYear();
-const yearBanned = document.querySelectorAll(".year");
-Array.from(yearBanned).forEach((banner) => (banner.innerText = year));
+document.addEventListener("DOMContentLoaded", () => {
+  const year = new Date().getFullYear();
+  const yearBanned = document.querySelectorAll(".year");
+  Array.from(yearBanned).forEach((banner) => (banner.innerText = year));
 
-const images = ["大阪街頭", "日本櫻花", "京都古城"];
-const backgroundImage = document.querySelector(".background-image");
-let imageIndex = 0;
-function rotateImage() {
+  const images = ["大阪街頭", "日本櫻花", "京都古城"];
+  const backgroundImage = document.querySelector(".background-image");
+  let imageIndex = 0;
+  function rotateImage() {
+    if (!backgroundImage) return;
     imageIndex >= images.length - 1 ? (imageIndex = 0) : imageIndex++;
     backgroundImage.style.backgroundImage = `url(./images/${images[imageIndex]}.jpg)`;
-}
-
-setInterval(rotateImage, 5000);
+  }
+  setInterval(rotateImage, 5000);
+});
